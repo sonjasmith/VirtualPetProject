06407ca1ba253601a6da5a7f43677d037f47a5b8
sonjasmith <sonjasmith@outlook.com>
2017-09-11 16:50:34 -0400

    First Commit

---
+++ VirtualPetProject/VirtualPet.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProject
+{
+    class VirtualPet
+
+    {
+        //VARIABLE declarations
+        private string name;
+        private string bodyColor;
+        private string beakColor;
+      
+        //CLASS CONSTRUCTOR declaration
+        //is used to set the initial state
+        public VirtualPet()
+        {
+            this.name = "GUI the Goose";
+            this.bodyColor = "Grey";
+            this.beakColor = "Orange";
+        }
+
+        //PROPERTIES (get and set)
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+
+        public string BodyColor
+        {
+            set { this.bodyColor = value; }
+            get { return this.bodyColor; }
+        }
+
+        public string BeakColor
+        {
+            set { this.beakColor = value; }
+            get { return this.beakColor; }
+        }
+
+        //METHODS
+        public void changePetName()
+        {
+            Console.WriteLine("Enter a new name: ");
+            Name = Console.ReadLine() + " GUI the Goose";
+            Console.WriteLine("Hello I'm " + Name + " and I'm ready to play!\n");
+        }
+        public void changeBodyColor()
+        {
+            Console.WriteLine("Enter a new body color: ");
+            BodyColor = Console.ReadLine();
+            Console.WriteLine(BodyColor + " is my favorite color!\n");
+        }
+
+        public void changeBeakColor()
+        {
+            Console.WriteLine("Enter a new beak color: ");
+            BeakColor = Console.ReadLine();
+            Console.WriteLine(BeakColor + " Wow look at you!\n ");
+        }
+    }
+}
---
+++ VirtualPetProject/VirturalPetMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProject
+{
+    class VirturalPetMain
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Screen
+            Console.WriteLine("Welcome to my virtual pet 1.0");
+            Console.WriteLine("Press Enter to interact with my pet goose.");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            //Instantiate VirtualPet
+            VirtualPet virtualPet = new VirtualPet();//petName is an instance of VirtualPet object
+
+            string strMenuChoice;
+            int menuChoice;
+
+            //diplay state
+            do
+            {
+                Console.WriteLine("My Virtual Pet is a goose.");
+                Console.WriteLine("His name is: " + virtualPet.Name);
+                Console.WriteLine("His body color is: " + virtualPet.BodyColor);
+                Console.WriteLine("His beak color is: " + virtualPet.BeakColor);
+                Console.WriteLine("\n");
+
+                //Menu layout
+                Console.WriteLine("Please make a selection of 1, 2 or 3 below: ");
+                Console.WriteLine();
+                Console.WriteLine("1) Change name");
+                Console.WriteLine("2) Change body color");
+                Console.WriteLine("3) Change beak color");
+                Console.WriteLine("4) Exit program");
+                Console.WriteLine("\n");
+
+                //Processed menu options
+                strMenuChoice = Console.ReadLine();
+                menuChoice = int.Parse(strMenuChoice);
+
+                switch (menuChoice)
+                {
+                    case 1:
+                        Console.WriteLine("You chose to change GUI's name");
+                        virtualPet.changePetName();
+                        break;
+                    case 2:
+                        Console.WriteLine("You chose a different body color.");
+                        virtualPet.changeBodyColor();
+                        break;
+                    case 3:
+                        Console.WriteLine("You chose to change your beak color.");
+                        virtualPet.changeBeakColor();
+                        break;
+                    case 4:
+                        Console.WriteLine("Good-bye sucka!");
+                        break;
+
+                    default:
+                        Console.WriteLine("I'm sorry, I don't understand that!");
+                        break;
+                }
+            }
+            while (menuChoice != 4);
+            System.Environment.Exit(0);
+        }
+    }
+ }
+
